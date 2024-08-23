110081f690536cb1ef70936c1480b655f2562b85
Aisa Lamdag <aisa.lamdag@pdax.ph>
2024-08-24 02:50:04 +0800

    added js and css

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Example of a simple interaction
+    document.querySelector('h1').addEventListener('click', function() {
+        alert('Welcome to My Portfolio!');
+    });
+});
