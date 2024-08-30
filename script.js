35b0c0a7aaf48994a221f44a5a6305f05984065b
Aisa Lamdag <aisa.lamdag@gmail.com>
2024-08-31 00:14:50 +0800

    added changes

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Example of a simple interaction
-    document.querySelector('h1').addEventListener('click', function() {
-        alert('Welcome to My Portfolio!');
-    });
-});
+
